38561d71df3a001515f045580a7372aceb319ab5
tejrdev <tejashkumar.r@uplers.in>
2023-05-19 20:17:19 +0530

    sw update

--- sw.js
+++ sw.js
@@ -2,16 +2,17 @@ self.addEventListener('install', e => {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
+       './',
        './videos.html',
        './blob.js',
-       './style.css'
+       './style.css',
      ]);
    })
  );
 });
 
 self.addEventListener('fetch', e => {
-  console.log(e.request.url);
+  //console.log(e.request.url);
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
   );
