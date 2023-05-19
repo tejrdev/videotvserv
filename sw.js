700fb7eabc27b3a3de90433beaaf8c453e67cb67
tejrdev <tejashkumar.r@uplers.in>
2023-05-19 20:06:09 +0530

    sw js change

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@ self.addEventListener('install', e => {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
-       './',
        './videos.html',
        './blob.js',
        './style.css'
